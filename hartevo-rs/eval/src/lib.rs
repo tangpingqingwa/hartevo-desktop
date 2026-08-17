c7c7af9694ccb86bf6fc0a10512e3de8087a65d8
Hartevo Integration Manager <integration@hartevo.invalid>
2026-08-17 12:50:51 +0800

    chore(eval): format protected merge exports

--- hartevo-rs/eval/src/lib.rs
+++ hartevo-rs/eval/src/lib.rs
@@ -4,8 +4,8 @@ extern crate self as hartevo_eval;
 
 #[path = "../examples/hartevo-browser-contract/digest.rs"]
 mod digest;
-mod evaluation_plugin;
 mod distribution;
+mod evaluation_plugin;
 mod harness_lab;
 #[path = "../examples/hartevo-browser-contract/model.rs"]
 mod model;
@@ -14,15 +14,15 @@ mod run_receipt;
 #[path = "../examples/hartevo-browser-contract/verifier.rs"]
 mod verifier;
 
+pub use distribution::{
+    export_public_key, generate_keypair, sign_file, validate_gate, verify_file,
+};
 pub use evaluation_plugin::{
     DurableEvaluationResultProvider, DurableEvaluationService, EVALUATION_PLUGIN_AUTHORITY,
     EVALUATION_PLUGIN_RELEASE_DECISION, EVALUATION_PLUGIN_SCHEMA_VERSION, EvaluationEvaluator,
     EvaluationEvidence, EvaluationEvidenceProvenance, EvaluationExecutionStatus,
     EvaluationMissionConsumer, EvaluationMissionView, EvaluationPluginService,
     EvaluationPluginState, EvaluationResult, EvaluationResultProvider,
-};
-pub use distribution::{
-    export_public_key, generate_keypair, sign_file, validate_gate, verify_file,
 };
 pub use harness_lab::{
     CandidateIdentity as HarnessCandidateIdentity, CaseObservation as HarnessCaseObservation,
